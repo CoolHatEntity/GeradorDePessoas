20c946bd928bbe325de7cf478881d479f93fe0e0
CoolHatEntity <matheus17menezes@gmail.com>
2021-10-17 01:34:25 -0300

    Terminada a InterFacePadrao

--- src/com/geradorDePessoas/ui/InterFacePadrao.java
+++ src/com/geradorDePessoas/ui/InterFacePadrao.java
@@ -45,92 +45,80 @@ public class InterFacePadrao extends JFrame{
         Conta conta = new Conta();
         conta.setCliente(cliente);
 
-        copiarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clip.setContents(new StringSelection(campoJsonCliente.getText()), null);
-                System.out.println("|=| Campo Json: " + campoJsonCliente.getText());
-            }
+        copiarButton.addActionListener(e -> {
+            Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clip.setContents(new StringSelection(campoJsonCliente.getText()), null);
+            System.out.println("|=| Campo Json: " + campoJsonCliente.getText());
         });
-        gerarNovoButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String nome = cliente.getNome();
-                String nomeSocial = cliente.getNome_social();
-                String cpf = cliente.getCpf();
-                String sobrenome = cliente.getSobrenome();
+        gerarNovoButton.addActionListener(e -> {
+            String nome = cliente.getNome();
+            String nomeSocial = cliente.getNome_social();
+            String cpf = cliente.getCpf();
+            String sobrenome = cliente.getSobrenome();
 
-                System.out.println(geraJsonPessoa(cliente, 1));
+            System.out.println(geraJsonPessoa(cliente, 1));
 
-                cliente.atualizaCpf();
-                cliente.atualizaNome();
-                cliente.atualizaNomeSocial();
-                cliente.atualizaSobrenome();
+            cliente.atualizaCpf();
+            cliente.atualizaNome();
+            cliente.atualizaNomeSocial();
+            cliente.atualizaSobrenome();
 
-                if(manterNomeRadioButton.isSelected()){
-                    cliente.setNome(nome);
-                }
-                if(manterNomeSocialRadioButton.isSelected()){
-                    cliente.setNome_social(nomeSocial);
-                }
-                if(manterCPFRadioButton.isSelected()){
-                    cliente.setCpf(cpf);
-                }
-                if(manterSobrenomeRadioButton.isSelected()){
-                    cliente.setSobrenome(sobrenome);
-                }
-
-                campoCpfCliente.setText(cliente.getCpf());
-                campoNomeCliente.setText(cliente.getNome());
-                campoNomeSocialCliente.setText(cliente.getNome_social());
-                campoSobrenomeCliente.setText(cliente.getSobrenome());
-
-                campoJsonCliente.setText(geraJsonPessoa(cliente, 2));
+            if(manterNomeRadioButton.isSelected()){
+                cliente.setNome(nome);
             }
+            if(manterNomeSocialRadioButton.isSelected()){
+                cliente.setNome_social(nomeSocial);
+            }
+            if(manterCPFRadioButton.isSelected()){
+                cliente.setCpf(cpf);
+            }
+            if(manterSobrenomeRadioButton.isSelected()){
+                cliente.setSobrenome(sobrenome);
+            }
+
+            campoCpfCliente.setText(cliente.getCpf());
+            campoNomeCliente.setText(cliente.getNome());
+            campoNomeSocialCliente.setText(cliente.getNome_social());
+            campoSobrenomeCliente.setText(cliente.getSobrenome());
+
+            campoJsonCliente.setText(geraJsonPessoa(cliente, 2));
         });
-        gerarNovoButton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String numAgencia = conta.getNumAgencia();
-                String numConta = conta.getNumConta();
+        gerarNovoButton1.addActionListener(e -> {
+            String numAgencia = conta.getNumAgencia();
+            String numConta = conta.getNumConta();
 
-                String nome = conta.getCliente().getNome();
-                String nomeSocial = conta.getCliente().getNome_social();
-                String cpf = conta.getCliente().getCpf();
-                String sobrenome = conta.getCliente().getSobrenome();
+            String nome = conta.getCliente().getNome();
+            String nomeSocial = conta.getCliente().getNome_social();
+            String cpf = conta.getCliente().getCpf();
+            String sobrenome = conta.getCliente().getSobrenome();
 
-                System.out.println(geraJsonConta(conta));
+            System.out.println(geraJsonConta(conta));
 
-                conta.atualizaNumConta();
-                conta.atualizaNumAgencia();
+            conta.atualizaNumConta();
+            conta.atualizaNumAgencia();
 
-                if(manterRadioButton1.isSelected()){
-                    conta.setNumConta(numConta);
-                }
-
-                if(manterRadioButton.isSelected()){
-                    conta.setNumAgencia(numAgencia);
-                }
-
-                campoNomeConta.setText(nome);
-                campoNomeSocialConta.setText(nomeSocial);
-                campoSobrenomeConta01.setText(sobrenome);
-                campoSobrenomeConta02.setText(sobrenome);
-                campoCpfConta.setText(cpf);
-                campoNumeroConta.setText(numConta);
-                campoNumeroAgencia.setText(numAgencia);
-
-                campoJsonConta.setText(geraJsonConta(conta));
+            if(manterRadioButton1.isSelected()){
+                conta.setNumConta(numConta);
             }
+
+            if(manterRadioButton.isSelected()){
+                conta.setNumAgencia(numAgencia);
+            }
+
+            campoNomeConta.setText(nome);
+            campoNomeSocialConta.setText(nomeSocial);
+            campoSobrenomeConta01.setText(sobrenome);
+            campoSobrenomeConta02.setText(sobrenome);
+            campoCpfConta.setText(cpf);
+            campoNumeroConta.setText(numConta);
+            campoNumeroAgencia.setText(numAgencia);
+
+            campoJsonConta.setText(geraJsonConta(conta));
         });
-        copiarButton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clip.setContents(new StringSelection(campoJsonConta.getText()), null);
-                System.out.println("|=| Campo Json: "+ campoJsonConta.getText());
-            }
+        copiarButton1.addActionListener(e -> {
+            Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clip.setContents(new StringSelection(campoJsonConta.getText()), null);
+            System.out.println("|=| Campo Json: "+ campoJsonConta.getText());
         });
 
     }
